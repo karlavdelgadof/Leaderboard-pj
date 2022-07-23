c275af05e6b406b68a4f7ccd8e29933154a42978
karlavdelgadof <karlavdelgadof@gmail.com>
2022-07-22 20:19:06 -0400

    Install pluggin for favicon

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 
 module.exports = {
   mode: 'development',
@@ -16,6 +17,13 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      { 
+        test: /\.(woff|woff2|eot|ttf|otf)$/i, type: 'asset/resource',
+      }, 
     ],
   },
   plugins: [
@@ -23,6 +31,7 @@ module.exports = {
       title: 'Development',
       template: './src/index.html',
     }),
+    new FaviconsWebpackPlugin('./src/favicon-32x32.png'),
   ],
   output: {
     filename: '[name].bundle.js',
